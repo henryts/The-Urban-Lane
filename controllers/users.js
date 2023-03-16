0c3e2b0d0074f9375939f3b6e04f1a6a7d595039
henry sathyan <henrysathyan@gmail.com>
2023-03-17 01:51:37 +0530

    checkout UI done

--- controllers/users.js
+++ controllers/users.js
@@ -431,11 +431,86 @@ getCart: async (req, res) => {
 //       res.redirect('/showCart');
 //     },
 
- checkOut: (req,res)=>{
+ checkOut:async (req,res)=>{
 
         if (req.session.loggedIn) {
-         let uid = req.session.userid ;       
-      res.render('user/CHECKOUT',{uid});  
+         let uEmail = req.session.userid.email ;  
+         const cart = await cartcollections.aggregate([
+          // match the cart based on the user's email
+          { $match: { userEmail: uEmail } },
+        
+          // unwind the product array
+          { $unwind: "$product" },
+        
+          // join with the products collection to get the product details
+          {
+            $lookup: {
+              from: "products",
+              localField: "product.pid",
+              foreignField: "_id",
+              as: "productDetails"
+            }
+          },
+        
+         // group the data by cart ID and product ID
+          {
+            $group: {
+              _id: { cartId: "$_id", productId: "$product.pid" },
+              userEmail: { $first: "$userEmail" },
+              product: { $first: "$product" },
+              productDetails: { $first: { $arrayElemAt: [ "$productDetails", 0 ] } }
+            }
+          },
+        
+          // group the data by cart ID
+          {
+            $group: {
+              _id: "$_id.cartId",
+              userEmail: { $first: "$userEmail" },
+              products: {
+                $push: {
+                  pid: "$product.pid",
+                  size: "$product.size",
+                  qty: "$product.qty",
+                  productTotal: "$productDetails.subTotal",
+                  productName: "$productDetails.productName",
+                  productDescription: "$productDetails.productDescription",
+                  brandName: "$productDetails.brandName",
+                  productCost: "$productDetails.productCost",
+                  productCatogory: "$productDetails.productCatogory",
+                  productImages: "$productDetails.productImages",
+                 
+                }
+              },
+             // totalPrice: { $sum: "$product.productTotal" }
+            }
+          },
+          {
+            $project: {
+              _id: 0,
+              userEmail: 1,
+              products: {
+                $map: {
+                  input: "$products",
+                  as: "p",
+                  in: {
+                    productsId:"$$p._id",
+                    productName: "$$p.productName",
+                    qty: "$$p.qty",
+                      productCost: "$$p.productCost",
+                    productImages: "$$p.productImages",
+                    subtotal: { $multiply: [{$toInt:"$$p.qty"}, {$toInt:"$$p.productCost"}] },
+                   
+                },
+                
+              },
+             //  totalPrice: { $sum: "$products.total" }
+                  }
+            }
+          }
+        ]).exec();    
+  
+      res.render('user/CHECKOUT',{uid, product: cart[0].products });  
         }
     },
         
