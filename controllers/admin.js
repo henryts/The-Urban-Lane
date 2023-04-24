3b7e6cd6658baba27a86cedaad7cd4dad9d9f848
henry sathyan <henrysathyan@gmail.com>
2023-04-24 07:44:13 +0530

    dashboard update

--- controllers/admin.js
+++ controllers/admin.js
@@ -65,30 +65,30 @@ console.log("orderStat",orderStat);
 //console.log("formattedMonthlyOrderCount",formattedMonthlyOrderCount);  
 //console.log("janary count in fotmatedmonth",formattedMonthlyOrderCount);
 const monthsToDisplay = ['2023-01', '2023-02', '2023-03', '2023-04', '2023-05', '2023-06','2023-07', '2023-08', '2023-09', '2023-10', '2023-11','2023-12'];
-if(formattedMonthlyOrderCount==null)
-{
-  var monthlyOrderCount = [];
-  monthlyOrderCount['2023-01']=0;
-  monthlyOrderCount['2023-02']=0;
-  monthlyOrderCount['2023-03']=0;
-  monthlyOrderCount['2023-04']=0;
-  monthlyOrderCount['2023-05']=0;
-  monthlyOrderCount['2023-06']=0;
-  monthlyOrderCount['2023-07']=0;
-  monthlyOrderCount['2023-08']=0;
-  monthlyOrderCount['2023-09']=0;
-  monthlyOrderCount['2023-10']=0;
-  monthlyOrderCount['2023-11']=0;
-  monthlyOrderCount['2023-12']=0;
-}
-else{
+// if(formattedMonthlyOrderCount==null)
+// {
+//   var monthlyOrderCount = [];
+//   monthlyOrderCount['2023-01']=0;
+//   monthlyOrderCount['2023-02']=0;
+//   monthlyOrderCount['2023-03']=0;
+//   monthlyOrderCount['2023-04']=0;
+//   monthlyOrderCount['2023-05']=0;
+//   monthlyOrderCount['2023-06']=0;
+//   monthlyOrderCount['2023-07']=0;
+//   monthlyOrderCount['2023-08']=0;
+//   monthlyOrderCount['2023-09']=0;
+//   monthlyOrderCount['2023-10']=0;
+//   monthlyOrderCount['2023-11']=0;
+//   monthlyOrderCount['2023-12']=0;
+// }
+// else{
 var monthlyOrderCount = monthsToDisplay.reduce((acc, month) => {
   const count = formattedMonthlyOrderCount[month] || 0;
   acc[month] = count;
   return acc;
 }, {});
 monthlyOrderCount['2023-01']=formattedMonthlyOrderCount['count'];
-}
+
 //console.log(monthlyOrderCount);
 //console.log("january count",monthlyOrderCount['2023-01']);
 
@@ -330,7 +330,7 @@ console.log("Total orders in current month:", totalOrders);
   } else {
     res.redirect("/admin/adminlogin");
   }
-  },
+},
 
   userlist: async (req, res) => {
     //user data listing
